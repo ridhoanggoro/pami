dcce1ac8b35d906b098eb3a9c58276223b2c9057
Ridho Anggoro <makhluktuhanpalingkeren@gmail.com>
2019-04-02 15:18:00 +0700

    Update header.php

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -28,7 +28,6 @@
   <!-- Dropzone Upload -->
   <link href='https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.4.0/dropzone.css' type='text/css' rel='stylesheet'>
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
   <!-- Select2 -->
   <link rel="stylesheet" href="<?php echo base_url('assets/vendor/select2/dist/css/select2.min.css');?>">
   <!-- Theme style -->
@@ -82,24 +81,18 @@
                         <li class="dropdown notifications-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
                           <i class="fa fa-envelope-o"></i>
-                          <span class="label label-info"><?php echo '10'; ?></span>
+                          <span class="label label-info"><?php echo $reg_stat; ?></span>
                         </a>
                         <ul class="dropdown-menu">
-                          <li class="header">You have 10 notifications</li>
+                          <li class="header">You have <?php echo $reg_stat;?> notifications</li>
                           <li>
                             <!-- inner menu: contains the actual data -->
                             <ul class="menu">
                               <li>
-                                <a href="#">
-                                  <i class="fa fa-users text-aqua"></i> 5 new members joined today
+                                <a href="<?php echo base_url("user/registered_list"); ?>">
+                                  <i class="fa fa-users text-aqua"></i> <?php echo $reg_stat;?> new members need approval
                                 </a>
-                              </li>
-                              <li>
-                                <a href="#">
-                                  <i class="fa fa-warning text-yellow"></i> Very long description here that may not fit into the
-                                  page and may cause design problems
-                                </a>
-                              </li>
+                              </li>                             
                               <li>
                                 <a href="#">
                                   <i class="fa fa-users text-red"></i> 5 new members joined
@@ -108,17 +101,11 @@
 
                               <li>
                                 <a href="#">
-                                  <i class="fa fa-shopping-cart text-green"></i> 25 sales made
+                                  <i class="fa fa-shopping-cart text-green"></i> 25 transaction made
                                 </a>
-                              </li>
-                              <li>
-                                <a href="#">
-                                  <i class="fa fa-user text-red"></i> You changed your username
-                                </a>
-                              </li>
+                              </li>                              
                             </ul>
-                          </li>
-                          <li class="footer"><a href="#">View all</a></li>
+                          </li>                         
                         </ul>
                         </li>
                         <?php } ?>
