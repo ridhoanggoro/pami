a7962a00fdfc97e1cacc080314484cbd6dab8c52
Ridho Anggoro <makhluktuhanpalingkeren@gmail.com>
2019-04-05 16:55:33 +0700

    added function register product

--- application/modules/trx/controllers/Trx.php
+++ application/modules/trx/controllers/Trx.php
@@ -33,4 +33,59 @@ class Trx extends CI_Controller {
         $this->load->view('overview', $data);
         $this->load->view('include/footer');
     }
+
+    public function product_list($id='')
+    {
+        $data['user_data'] = '';
+        $this->load->view('include/header'); 
+        $this->load->view('product_list', $data);
+        $this->load->view('include/footer');
+    }
+
+    public function dataTable (){
+        is_login();
+        $table = 'product';
+        $primaryKey = 'product_id';
+        $columns = array(
+                    array( 'db' => 'product_id', 'dt' => 0 ),
+                    array( 'db' => 'product_id', 'dt' => 1 ),
+                    array( 'db' => 'name', 'dt' => 2 ),
+                    array( 'db' => 'type', 'dt' => 3 ),
+                    array( 'db' => 'status', 'dt' => 4 ),
+                    array( 'db' => 'product_id', 'dt' => 5 )
+        );
+
+        $sql_details = array(
+            'user' => $this->db->username,
+            'pass' => $this->db->password,
+            'db'   => $this->db->database,
+            'host' => $this->db->hostname
+        );
+        //$where = array("status = 1");
+        $where = '';
+        $output_arr = SSP::complex( $_GET, $sql_details, $table, $primaryKey, $columns, $where);
+        foreach ($output_arr['data'] as $key => $value) {
+            $id = $output_arr['data'][$key][count($output_arr['data'][$key])  - 1];
+            $output_arr['data'][$key][count($output_arr['data'][$key])  - 1] = '';
+            if(CheckPermission('user', "all_update")){
+            $output_arr['data'][$key][count($output_arr['data'][$key])  - 1] .= '<a id="btnEditRow" class="modalButtonProduct mClass"  href="javascript:;" type="button" data-src="'.$id.'" title="Rubah"><i class="fa fa-pencil" data-id=""></i></a>';
+            }else if(CheckPermission('user', "own_update") && (CheckPermission('user', "all_update")!=true)){
+                $user_id =getRowByTableColomId($table,$id,'users_id','user_id');
+                if($user_id==$this->user_id){
+            $output_arr['data'][$key][count($output_arr['data'][$key])  - 1] .= '<a id="btnEditRow" class="modalButtonProduct mClass"  href="javascript:;" type="button" data-src="'.$id.'" title="Edit"><i class="fa fa-pencil" data-id=""></i></a>';
+                }
+            }
+            
+            if(CheckPermission('user', "all_delete")){
+            $output_arr['data'][$key][count($output_arr['data'][$key])  - 1] .= '<a style="cursor:pointer;" data-toggle="modal" class="mClass" onclick="setId('.$id.', \'product\')" data-target="#cnfrm_delete" title="delete"><i class="fa fa-trash-o" ></i></a>';}
+            else if(CheckPermission('user', "own_delete") && (CheckPermission('user', "all_delete")!=true)){
+                $user_id =getRowByTableColomId($table,$id,'users_id','user_id');
+                if($user_id==$this->user_id){
+            $output_arr['data'][$key][count($output_arr['data'][$key])  - 1] .= '<a style="cursor:pointer;" data-toggle="modal" class="mClass" onclick="setId('.$id.', \'product\')" data-target="#cnfrm_delete" title="delete"><i class="fa fa-trash-o" ></i></a>';
+                }
+            }
+            $output_arr['data'][$key][0] = '<input type="checkbox" name="selData" value="'.$output_arr['data'][$key][0].'">';
+        }
+        echo json_encode($output_arr);
+    }
 }
--- application/modules/trx/views/portfolio.php
+++ application/modules/trx/views/portfolio.php
@@ -15,23 +15,27 @@
 	            </div>
 	          </div>
 	          <!-- /.box-header -->
-	          <div class="box-body">           
-						<div class="container overview-content pad-bottom-30" id="overview-list" style="margin-bottom: 30px; margin-top: 0px !important;">
-							<div class="col-sm-5 portfolio-right-div padding-0 hidden" id="jumpDiv">
-									<div class="col-sm-9 col-sm-offset-3 portfolio-jump-sbn" id="jumpInnerDiv">    
-											<div class="row portfolio-jump-item clickable-jump">
-													<div class="col-sm-2">
-															<img class="portfolio-jump-logo" src="images/icon_sbn.png">
-													</div>
-													<div class="col-sm-10 text-center portfolio-jump-text-div">
-															<span class="portfolio-jump-text">LIHAT PORTOFOLIO SURAT BERHARGA NEGARA</span>
-													</div>
-											</div>        
-									</div>
-							</div>
-							<div class="col-xs-12" id="list-container" style="margin-top: 20px;"><div class="col-xs-12 portfolio-header hidden-xs"><div class="row"><div class="hidden-sm col-sm-1"><span>Manajer Investasi</span></div><div class="col-sm-2"><span>Nama Produk</span></div><div class="col-sm-2"><span>Nilai Saat Ini</span></div><div class="col-md-2 col-sm-2"><span>Modal Beli</span></div><div class="col-sm-1"><span>Potensi Laba/Rugi</span></div><div class="col-sm-2"><span>NAB/Unit</span></div><div class="col-sm-1"><span>Total Unit<br>Kepemilikan</span></div><div class="col-sm-1"><span>Unit Tersedia<br>Untuk Dijual</span></div></div></div><div class="col-xs-12 portfolio-empty-col"><img src="images/account/ilus-eggbasket.png"><span>Anda belum membeli reksa dana apa pun.<br>Ingatlah untuk tidak menaruh semua telur Anda dalam satu keranjang!</span><button class="btn" onclick="window.location.href=$('#baseURL').val() + '/product/productlist';">MULAI INVESTASI</button></div></div>
-							</div>
-						</div>
+	          <div class="box-body table-responsive no-padding">
+              <table class="table table-hover">
+                <tbody>
+								<tr>
+                  <th>Nama Produk</th>
+                  <th>Nilai Saat Ini</th>
+                  <th>Modal Beli</th>
+                  <th>Potensi Laba/Rugi</th>
+                  <th>NAB/Unit</th>
+									<th>Total Unit Kepemilikan</th>
+									<th>Unit Tersedia Untuk Dijual</th>
+                </tr>
+                <tr>
+                  <td colspan="7">Anda belum membeli reksa dana apa pun.</td>                  									
+                </tr>   
+								<tr>
+                  <td colspan="7"><a href="<?php echo base_url('trx/product_list');?>" class="btn btn-primary btn-block margin-bottom">Mulai Investasi</a></td>                  									
+                </tr>             
+              </tbody>
+              </table>
+            </div>
 	          <!-- /.box-body -->
 	        </div>
 	        <!-- /.box -->
